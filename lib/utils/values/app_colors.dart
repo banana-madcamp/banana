8e134115820bfca5673642505dbba8fb94d5c732
unripe_plum <khjalk8625@kaist.ac.kr>
2025-07-04 18:22:24 +0900

    ui; icons and colors

---
+++ lib/utils/values/app_colors.dart
@@ -0,0 +1,8 @@
+class AppColors{
+  static const primary = 0xFFFF8383;
+  static const logo = 0xFFFF4056;
+  static const gray = 0xFFBDBDBD;
+  static const lightGray = 0xFFF2F2F7;
+  static const black = 0xFF000000;
+  static const white = 0xFFFFFFFF;
+}

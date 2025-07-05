246a639ac7809e410a79775bea5f72c828d5f12d
unripe_plum <khjalk8625@kaist.ac.kr>
2025-07-05 16:17:38 +0900

    ui; product information

--- lib/main/views/main_product_add_view.dart
+++ lib/main/views/main_product_add_view.dart
@@ -6,8 +6,145 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class MainProductAddView extends StatelessWidget {
+class MainProductAddView extends StatefulWidget {
   const MainProductAddView({super.key});
+
+  @override
+  State<MainProductAddView> createState() => _MainProductAddViewState();
+}
+
+class _MainProductAddViewState extends State<MainProductAddView> {
+  final titleController = TextEditingController();
+  final subTitleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final priceController = TextEditingController();
+  final tradingAreaController = TextEditingController();
+
+  Widget titleTextField() {
+    return TextFormField(
+      controller: titleController,
+      decoration: InputDecoration(
+        labelStyle: TextStyle(
+          fontSize: 28,
+          color: AppColors.gray,
+          fontFamily: 'Rubik',
+          fontWeight: FontWeight.normal,
+        ),
+        labelText: 'Title',
+      ),
+    );
+  }
+
+  Widget subTitleTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelStyle: TextStyle(
+          fontSize: 14,
+          color: AppColors.gray,
+          fontFamily: 'Rubik',
+          fontWeight: FontWeight.normal,
+        ),
+        labelText: 'Subtitle',
+      ),
+    );
+  }
+
+  Widget descriptionTextField() {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.lightGray,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: TextFormField(
+        controller: descriptionController,
+        maxLines: 5,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(left: 12, right: 12, top: 24),
+          border: InputBorder.none,
+          hintText: '- 브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요.',
+          hintStyle: TextStyle(
+            wordSpacing: -0.02,
+            fontSize: 14,
+            color: AppColors.gray,
+            fontFamily: 'Rubik',
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget priceTextField() {
+    return TextFormField(
+      controller: priceController,
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: "enter your price",
+        hintStyle: TextStyle(
+          color: AppColors.gray,
+          fontSize: 14,
+          fontFamily: 'Rubik',
+          fontWeight: FontWeight.normal,
+        ),
+        prefixIconConstraints: BoxConstraints(minWidth: 16, maxHeight: 16),
+        prefixIcon: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(AppIcons.price, color: AppColors.gray, size: 16),
+            VerticalDivider(color: AppColors.gray, thickness: 1.5),
+          ],
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColors.gray, width: 1.5),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColors.primary, width: 2),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) return 'please enter your price';
+        if ((!value.isNum) || ((int.tryParse(value) ?? -1) < 0))
+          return 'it is not a valid number';
+        return null;
+      },
+    );
+  }
+
+  Widget tradingAreaTextField() {
+    return TextFormField(
+      controller: tradingAreaController,
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: "enter your trading area",
+        hintStyle: TextStyle(
+          color: AppColors.gray,
+          fontSize: 14,
+          fontFamily: 'Rubik',
+          fontWeight: FontWeight.normal,
+        ),
+        prefixIconConstraints: BoxConstraints(minWidth: 16, maxHeight: 16),
+        prefixIcon: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(AppIcons.location, color: AppColors.gray, size: 16),
+            VerticalDivider(color: AppColors.gray, thickness: 1.5),
+          ],
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColors.gray, width: 1.5),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColors.primary, width: 2),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) return 'please enter your price';
+        if ((!value.isNum) || ((int.tryParse(value) ?? -1) < 0))
+          return 'it is not a valid number';
+        return null;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,28 +198,8 @@ class MainProductAddView extends StatelessWidget {
               ),
             ),
             SizedBox(height: 15),
-            TextFormField(
-              decoration: InputDecoration(
-                labelStyle: TextStyle(
-                  fontSize: 28,
-                  color: AppColors.gray,
-                  fontFamily: 'Rubik',
-                  fontWeight: FontWeight.normal,
-                ),
-                labelText: 'Title',
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelStyle: TextStyle(
-                  fontSize: 14,
-                  color: AppColors.gray,
-                  fontFamily: 'Rubik',
-                  fontWeight: FontWeight.normal,
-                ),
-                labelText: 'Subtitle',
-              ),
-            ),
+            titleTextField(),
+            subTitleTextField(),
             SizedBox(
               height: 36,
               child: Row(
@@ -100,31 +217,7 @@ class MainProductAddView extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: AppColors.lightGray,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: TextFormField(
-                maxLines: 5,
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.only(
-                    left: 12,
-                    right: 12,
-                    top: 24,
-                  ),
-                  border: InputBorder.none,
-                  hintText: '- 브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요.',
-                  hintStyle: TextStyle(
-                    wordSpacing: -0.02,
-                    fontSize: 14,
-                    color: AppColors.gray,
-                    fontFamily: 'Rubik',
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ),
+            descriptionTextField(),
             SizedBox(
               height: 36,
               child: Row(
@@ -160,6 +253,25 @@ class MainProductAddView extends StatelessWidget {
                 ],
               ),
             ),
+            priceTextField(),
+            SizedBox(
+              height: 36,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Trading Area',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: AppColors.black,
+                      fontFamily: 'Rubik',
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            tradingAreaTextField(),
             Expanded(child: Container()),
             MainBottomButton(onPressed: () {}, text: "LIST A PRODUCT"),
             SizedBox(height: 20),
